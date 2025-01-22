dc4f7e2f35b2ced60202e01d9d998ceda227cc99
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2025-01-22 08:48:54 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import UpperHeader, { Header } from "@/components/header";
-import Footer from "@/components/footer";
+import UpperHeader, { Header } from "../components/header";
+import Footer from "../components/footer";
 import { CartProvider } from "./CONTEXT/cartContext";
 
 const geistSans = localFont({
