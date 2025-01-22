137396ce8b5274e72064a4612087050fde97f97f
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2025-01-22 07:49:31 +0500

    Delete sanity directory

--- sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { productSchema } from './product'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema],
-}
